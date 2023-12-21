c8e0967ccd0a5b558744c51de0849e11833d0efa
faryek <ermak1871@gmail.com>
2023-12-22 02:46:09 +0500

    fix

--- landscript.js
+++ landscript.js
@@ -19,7 +19,15 @@ for (let i = 0; i < layers.length; i++){
     layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0.7);"
     texts[i].style.cssText += "color: rgba(255, 180, 67, 1);"
   })
+  texts[i].addEventListener("mouseover", () => {
+    layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0.7);"
+    texts[i].style.cssText += "color: rgba(255, 180, 67, 1);"
+  })
   layers[i].addEventListener("mouseout", () => {
+    layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0);"
+    texts[i].style.cssText += "color: rgba(255, 180, 67, 0);"
+  })
+  texts[i].addEventListener("mouseout", () => {
     layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0);"
     texts[i].style.cssText += "color: rgba(255, 180, 67, 0);"
   })

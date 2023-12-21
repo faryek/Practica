3bef0d991615bf922b102ea9f26877693882ee36
faryek <ermak1871@gmail.com>
2023-12-21 23:13:47 +0500

    cards ready

--- landscript.js
+++ landscript.js
@@ -8,3 +8,23 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
+
+let layers = document.getElementsByClassName("card-layer")
+let texts = document.getElementsByClassName("card-text")
+
+
+
+for (let i = 0; i < layers.length; i++){
+  layers[i].addEventListener("mouseover", () => {
+    layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0.7);"
+    texts[i].style.cssText += "color: rgba(255, 180, 67, 1);"
+  })
+  layers[i].addEventListener("mouseout", () => {
+    layers[i].style.cssText += "background-color: rgba(0, 0, 0, 0);"
+    texts[i].style.cssText += "color: rgba(255, 180, 67, 0);"
+  })
+}
+
+
+
+
